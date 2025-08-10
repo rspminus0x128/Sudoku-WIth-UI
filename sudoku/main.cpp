3968fecc778894685f43cf3c5437ece63a23a9bf
rspminus0x128 <rspminus0x128@tutanota.com>
2025-08-09 20:53:59 -0500

    Add correct entry point for debug/release
    Uses main() for debugging to preserve the console, uses WinMain() for release builds to hide the console.

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -1,16 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
 #include "sudoku_rendering.hpp"
 #include "sudoku_board.hpp"
 #include "game.hpp"
 
-// switch to WinMain in release mode
-int main() {
+int runGame() {
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 800)), "Sudoku");
     SudokuBoard board;
     SudokuRenderer renderer(window, board);
     SudokuGame game(window, board, renderer);
     renderer.game = &game;
 
+    window.setFramerateLimit(60);
     while (window.isOpen()) {
         while (const std::optional<sf::Event> event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
@@ -25,3 +26,10 @@ int main() {
 
     return 0;
 }
+
+#ifdef _DEBUG
+int main() { return runGame(); }
+#else
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return runGame(); }
+#endif
